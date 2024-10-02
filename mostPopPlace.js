62f8cd34110ebba43f5849b8ee178c39aec80d53
mihago96 <hei.mihago.2@gmail.com>
2024-10-02 15:07:38 +0300

    feat: methode of most popular 80% fonctionnal

--- mostPopPlace.js
+++ mostPopPlace.js
@@ -1,7 +1,13 @@
 export function getMostPopularPlaces(places){
     let x = [];
-   
-    return places;
+   for(let i = 0; i<places.length;i++){
+    if(places[i]==places[i+1]){
+        x.push(places[i])
+    }
+    
+   }
+
+    return x.join(' ').toString();
 }
 
 console.log(getMostPopularPlaces(["Analamahitsy","Analamahitsy","Itaosy","Behoririka","Itaosy","Itaosy"]));
